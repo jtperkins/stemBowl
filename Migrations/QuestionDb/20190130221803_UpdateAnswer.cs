4beb61d5f776059601810b63165f19d6a6ec1022
cslaf <cade.schlaefli@gmail.com>
2019-01-30 16:20:57 -0600

    changing db model

---
+++ Migrations/QuestionDb/20190130221803_UpdateAnswer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace stembowl.Migrations.QuestionDb
+{
+    public partial class UpdateAnswer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Correct",
+                table: "Answer",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Correct",
+                table: "Answer");
+        }
+    }
+}
--- Models/Answer.cs
+++ Models/Answer.cs
@@ -6,6 +6,7 @@ namespace stembowl.Models
     {
         public int AnswerID {get; set;}
         public int QuestionID {get; set;}
-        public string Text{ get; set;}
+        public string Text {get; set;}
+        public bool Correct {get; set;}
     }
 }
