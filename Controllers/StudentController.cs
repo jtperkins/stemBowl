c3d135cae25c2156dd689dd73453b03029d51e9f
JTPerkins <heershey@outlook.com>
2019-02-16 21:50:58 -0600

    implemented displaying quiz round interface and check score

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -12,7 +12,6 @@ using System.Security.Authentication;
 using System.Security.Principal;
 using Shared.Web.MvcExtensions;
 using stembowl.Areas.Identity;
-using Microsoft.EntityFrameworkCore;
 
 
 namespace stembowl.Controllers
@@ -46,18 +45,48 @@ namespace stembowl.Controllers
             return View(question);
         }
 
+        [HttpPost]
+        public IActionResult Index(TeamAnswers answers)
+        {
+            var correct = _context.Answer
+                .Where(e => e.QuestionID == answers.QuestionID && e.Correct)
+                .FirstOrDefault();
+
+            if (answers.Answer.Equals(correct.Text))
+                answers.Correct = true;
+            else
+                answers.Correct = false;
+
+            _context.TeamAnswers.Update(answers);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public IActionResult EndOfRound()
         {
             return View();  
         }
 
-        [HttpPost]
-        public IActionResult Index(TeamAnswers answers)
+        public async Task<IActionResult> CheckScore()
         {
-            _context.TeamAnswers.Update(answers);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
+            double score = 0;
+            
+            var user = await _userContext.GetUserAsync(User);
+            var questions = _context.TeamAnswers
+                .Where(e => e.TeamID == user.TeamID);
+                
+            foreach(var question in questions)
+            {
+                if (question.Correct)
+                score++;
+            }
+
+            double percentage = (score / questions.Count()) * 100 ;
+
+            return View(percentage);  
         }
+
+        
 
 
     }
